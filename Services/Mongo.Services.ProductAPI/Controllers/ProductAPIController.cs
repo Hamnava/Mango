c146d9daca97e5a6a5b483b33c4fa78e873b1fe4
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-10-27 20:04:08 +0430

    more update product service

---
+++ Services/Mongo.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mongo.Services.ProductAPI.Models.Dto;
+using Mongo.Services.ProductAPI.Repository;
+
+namespace Mongo.Services.ProductAPI.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+        private readonly IProductRepository _product;
+        protected ResponseDto _response;
+
+        public ProductAPIController(IProductRepository product)
+        {
+            _product = product;
+            this._response = new ResponseDto();
+        }
+
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _product.GetProducts();
+                _response.Result = productDtos;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return _response;
+        }
+
+
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                ProductDto productDto = await _product.GetProductById(id);
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return _response;
+        }
+
+
+        [HttpPost]
+        public async Task<object> Create([FromBody] ProductDto model)
+        {
+            try
+            {
+                ProductDto productDto = await _product.CraeteUpdateProduct(model);
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return _response;
+        }
+
+
+        [HttpPut]
+        public async Task<object> Update([FromBody] ProductDto model)
+        {
+            try
+            {
+                ProductDto productDto = await _product.CraeteUpdateProduct(model);
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return _response;
+        }
+
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<object> Delete(int id)
+        {
+            try
+            {
+                bool isSuccess = await _product.DeleteProduct(id);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return _response;
+        }
+    }
+}
--- Services/Mongo.Services.ProductAPI/Program.cs
+++ Services/Mongo.Services.ProductAPI/Program.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Mongo.Services.ProductAPI;
 using Mongo.Services.ProductAPI.DbContexts;
+using Mongo.Services.ProductAPI.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,7 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 
 // Add services to the container.
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
