b04d2f8328427a9bc9ae777b995ce2ca34da545d
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-10-27 17:33:52 +0430

    Add project files.

---
+++ Services/Mongo.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mongo.Services.ProductAPI.Models;
+
+namespace Mongo.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
