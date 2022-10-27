0b34e1df5fe18426a1d61e7f53ad79d514f7e40d
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-10-27 19:09:11 +0430

    repository implementation done

---
+++ Services/Mongo.Services.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Mongo.Services.ProductAPI.DbContexts;
+using Mongo.Services.ProductAPI.Models;
+using Mongo.Services.ProductAPI.Models.Dto;
+
+namespace Mongo.Services.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public ProductRepository(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<ProductDto> CraeteUpdateProduct(ProductDto productDto)
+        {
+            Product product = _mapper.Map<ProductDto, Product>(productDto);
+            if (product.ProductId > 0)
+            {
+                _context.Products.Update(product);
+            }
+            else
+            {
+                _context.Products.Add(product);
+            }
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<Product, ProductDto>(product);
+        }
+
+        public async Task<bool> DeleteProduct(int productId)
+        {
+            Product product = await _context.Products.Where(x => x.ProductId == productId).FirstOrDefaultAsync();
+            if(product == null)
+            {
+                return false;
+            }
+             _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<ProductDto> GetProductById(int productId)
+        {
+            Product product = await _context.Products.Where(x => x.ProductId == productId).FirstOrDefaultAsync();
+            return _mapper.Map<ProductDto>(product);
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetProducts()
+        {
+            List<Product> products = await _context.Products.ToListAsync();
+            return _mapper.Map<List<ProductDto>>(products);
+         }
+    }
+}
