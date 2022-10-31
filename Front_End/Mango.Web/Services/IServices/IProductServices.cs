4bc994fb8020eecdc9f5f30284f37bcd65a5fcab
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-10-31 11:34:18 +0430

    webUi updated

---
+++ Front_End/Mango.Web/Services/IServices/IProductServices.cs
@@ -0,0 +1,13 @@
+﻿using Mongo.Web.Models;
+
+namespace Mango.Web.Services.IServices
+{
+    public interface IProductServices
+    {
+        Task<T> GetAllProductsAsync<T>();
+        Task<T> GetProductByIdAsync<T>(int id);
+        Task<T> CreateProductsAsync<T>(ProductDto productDto);
+        Task<T> UpdateProductsAsync<T>(ProductDto productDto);
+        Task<T> DeleteProductsAsync<T>(int id);
+    }
+}
