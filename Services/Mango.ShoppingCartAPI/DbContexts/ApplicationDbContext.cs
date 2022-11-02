3e9c1c215b35084dec1a4514bf7254a6e64ce1ae
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-11-02 16:29:07 +0430

    shopping cart project setuped

---
+++ Services/Mango.ShoppingCartAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mango.ShoppingCartAPI.DbContexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+
+        }
+
+        //public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ Services/Mango.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace Mango.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}
